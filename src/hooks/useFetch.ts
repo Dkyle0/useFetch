1668324d4a55701e39268c35004f20c64552c930
Dkyle0 <50553897+Dkyle0@users.noreply.github.com>
2024-08-02 11:40:45 +0300

    add useCallback

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,50 +1,54 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from 'react';
 
 interface UseFetchReturnType<T> {
-  data: T[];
-  isLoading: boolean;
-  error: Error | undefined;
-  refetch: (options?: RefetchOptions) => void;
+	data: T[];
+	isLoading: boolean;
+	error: Error | undefined;
+	refetch: (options?: RefetchOptions) => void;
 }
 
 interface RefetchOptions {
-  params?: { [key: string]: any };
+	params?: { [key: string]: any };
 }
 
 export const useFetch = <T>(url: string): UseFetchReturnType<T> => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState<T[]>([]);
-  const [error, setError] = useState<Error | undefined>(undefined);
-  const [fetchUrl, setFetchUrl] = useState(url);
+	const [isLoading, setIsLoading] = useState(true);
+	const [data, setData] = useState<T[]>([]);
+	const [error, setError] = useState<Error | undefined>(undefined);
+	const [fetchUrl, setFetchUrl] = useState(url);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch(fetchUrl);
-        const fetchedData = await response.json();
-        setData(fetchedData);
-      } catch (e: unknown) {
-        if (e instanceof Error) {
-          setError(e);
-        } else {
-          setError(new Error("Error"));
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchData();
-  }, [fetchUrl]);
+	const fetchData = useCallback(async () => {
+		setIsLoading(true);
+		try {
+			const response = await fetch(fetchUrl);
+			const fetchedData = await response.json();
+			setData(fetchedData);
+		} catch (e: unknown) {
+			if (e instanceof Error) {
+				setError(e);
+			} else {
+				setError(new Error('Error'));
+			}
+		} finally {
+			setIsLoading(false);
+		}
+	}, [fetchUrl]);
 
-  const refetch = (options?: RefetchOptions) => {
-    if (options?.params) {
-      const queryParams = new URLSearchParams(options.params).toString();
-      setFetchUrl(`${url}?${queryParams}`);
-    } else {
-      setFetchUrl(url);
-    }
-  };
+	useEffect(() => {
+		fetchData();
+	}, [fetchData]);
 
-  return { data, isLoading, error, refetch };
+	const refetch = useCallback(
+		(options?: RefetchOptions) => {
+			if (options?.params) {
+				const queryParams = new URLSearchParams(options.params).toString();
+				setFetchUrl(`${url}?${queryParams}`);
+			} else {
+				setFetchUrl(url);
+			}
+		},
+		[url],
+	);
+
+	return { data, isLoading, error, refetch };
 };
